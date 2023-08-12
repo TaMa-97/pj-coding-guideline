c6b3a45b8a6a22869365405690ac55358c559e0f
Lilly <myk19977@gmail.com>
2023-08-12 18:57:54 +0900

    🔥 search Edit placeholder

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,7 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>コーディングガイドライン</span>,
+  search: { placeholder: "検索する" },
   footer: {
     text: "Coding Guideline",
   },
